08ccf48e96edbc077b4d5f424dd636dc08497946
Lucas Nahuel Raffo <lucasnahuelraffo@example.com>
2026-04-14 02:16:30 +0200

    fix: remove deleted partner logos and add new branding logo

--- src/Section3Content.jsx
+++ src/Section3Content.jsx
@@ -3,17 +3,13 @@ import MarqueeLogoScroller from './MarqueeLogoScroller';
 
 import logoBarbara from './img/Logos de Empresas/Barbara Chavez.png';
 import logoOrlando from './img/Logos de Empresas/Dr. Orlando Santillan.png';
-import logoHuawei from './img/Logos de Empresas/Huawei.png';
 import logoLuar from './img/Logos de Empresas/Luar.png';
-import logoNovocentro from './img/Logos de Empresas/Novocentro.png';
 import logoRenuev from './img/Logos de Empresas/Renuev.png';
 
 const partners = [
   { src: logoBarbara, alt: 'Barbara Chavez' },
   { src: logoOrlando, alt: 'Dr. Orlando Santillan' },
-  { src: logoHuawei, alt: 'Huawei' },
   { src: logoLuar, alt: 'Luar' },
-  { src: logoNovocentro, alt: 'Novocentro' },
   { src: logoRenuev, alt: 'Renuev' },
 ];
 
